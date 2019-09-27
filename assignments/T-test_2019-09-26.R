b4ab6e49f7b115fcec45f353ff82dabf620016de
michellekanik <michellemaja97@yahoo.com>
2019-09-27 15:09:05 -0400

    i think i did it

--- assignments/T-test_2019-09-26.R
+++ assignments/T-test_2019-09-26.R
@@ -26,3 +26,40 @@ ggplot(birth_diff_data) +
 
 ggplot(birth_diff_data)+
   geom_boxplot(aes(x = "", y = birth_diff))
+
+
+
+
+# scenario_2
+
+# Clean up the working environment
+rm(list = ls())
+# Verify working directory, should be ~/Documents/Analyses/lastname_first
+getwd()
+
+# Load tidyverse
+library("tidyverse")
+# Check for updates
+tidyverse_update()
+library(readr)
+horn <- read_csv("datasets/abd/chapter12/chap12e3HornedLizards.csv")
+View(horn)
+horn <- horn %>% slice (-105)
+
+
+survival_summ <- horn %>%
+  summarise(
+    mean_resp = mean(squamosalHornLength),
+    median_resp = median(squamosalHornLength),
+    IQR_resp = IQR(squamosalHornLength),
+    sd_resp = sd(squamosalHornLength),
+    var_resp = var(squamosalHornLength)
+  )
+
+
+# Compare the histograms and boxplots of horn and squareroot_eggs
+ggplot(horn) +
+  geom_histogram(aes(squamosalHornLength), binwidth = 1)
+
+ggplot(horn)+
+  geom_boxplot(aes(x = Survival, y = squamosalHornLength))
