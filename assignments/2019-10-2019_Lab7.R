43ddb7b31e52fb837136b957c20a985edeed1dd6
michellekanik <michellemaja97@yahoo.com>
2019-11-05 00:55:27 -0500

    hi

--- assignments/2019-10-2019_Lab7.R
+++ assignments/2019-10-2019_Lab7.R
@@ -88,4 +88,46 @@ summ_Aldrin <- waterwaste %>%
 #ratio
 ratio <-(max(summ_Aldrin$sd_Aldrin))/(min(summ_Aldrin$sd_Aldrin))
 
+autoplot(model02)
+anova(model02)
+summary(model02)
+
 # not specified so you would do unplanned
+# Unplanned comparisons
+# The key things you need to specify here are the model name and the factor name
+
+tukey <- glht(model02, linfct = mcp(Depth = "Tukey"))
+summary(tukey)
+
+
+### HCB ####
+ggplot(waterwaste, aes(x = Depth, y = HCB))+
+  geom_boxplot() +
+  theme_bw() 
+ggplot(waterwaste) +
+  geom_histogram(aes(HCB), binwidth = 0.7)+
+  facet_wrap(~Depth)
+ggplot(waterwaste)+
+  geom_qq(aes(sample = HCB, color = Depth))
+
+### Step 2.  Construct your ANOVA 
+
+# Similar to our two-sample t-test, you specify an equation (y~x) and the data
+
+model03 <- lm(HCB~Depth, data = waterwaste)
+
+### Step 3.  Check the assumptions, again 
+
+summ_HCB <- waterwaste %>%
+  group_by(Depth) %>% 
+  summarise(mean_HCB = mean(HCB),
+            sd_HCB = sd(HCB),
+            n_HCB = n())
+
+ratio <-(max(summ_HCB$sd_HCB))/(min(summ_HCB$sd_HCB))
+
+# ratio is under 3 so we do not have to mutate
+autoplot(model03)
+anova(model03)
+summary(model03)
+
